d8643d843bc6e295ae3cc489550637c3dcc3a5fa
njbultman <njbultman74@gmail.com>
2021-06-11 14:28:06 -0500

    Slight unit test comment changes.

--- tests/date_time/test_core.py
+++ tests/date_time/test_core.py
@@ -1,7 +1,9 @@
 from pyxlsxfunctions.date_time.core import NOW, TODAY
 
+# Tests for NOW function
 def test_NOW():
     assert type(NOW()) == type('known string')
 
+# Tests for TODAY function
 def test_TODAY():
     assert type(TODAY()) == type('known string')
